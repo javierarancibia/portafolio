32778c7a88104a321ce45d91ccc5226dec16c9d7
Javier <javier.arancibia.reyes@hotmail.com>
2020-08-17 15:57:29 -0400

    octavo commit prueba portafolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,22 @@ $('#myModal').modal(options)
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  $(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var link = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(link).offset().top
+          }, 800, function(){
+            window.location.hash = link;
+          });
+      } 
+    });
+  
+
+       
